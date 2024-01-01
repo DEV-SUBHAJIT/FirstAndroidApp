42aa972d182a9ea068ae130b7fe9000c4ec22b0b
DELL <adindia2023@gmail.com>
2024-01-01 20:36:10 +0530

    01 Jan 2024 Task

--- app/src/main/java/com/example/firstapp/activity/NewsDetailsActivity.java
+++ app/src/main/java/com/example/firstapp/activity/NewsDetailsActivity.java
@@ -10,11 +10,16 @@ import com.example.firstapp.model.News;
 
 public class NewsDetailsActivity extends AppCompatActivity {
 
+    News news;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
 
+        news = getIntent().getParcelableExtra("news");
+
+        if (news != null)
+            Toast.makeText(this, "" + news.getTitle(), Toast.LENGTH_SHORT).show();
     }
 }
